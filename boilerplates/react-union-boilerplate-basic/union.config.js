c4a1254f3567dd5c1ecfd32e72a0794b9dd7e2d1
Tomáš Konrády <tomaskonrady@gmail.com>
2018-01-16 16:00:02 +0100

    Fixing example

--- boilerplates/react-union-boilerplate-basic/union.config.js
+++ boilerplates/react-union-boilerplate-basic/union.config.js
@@ -1,10 +1,6 @@
 const path = require('path');
 
 module.exports = {
-	proxy: {
-		port: 3330,
-		target: 'http://localhost:8080',
-	},
 	devServer: {
 		port: 3300,
 		baseDir: path.resolve(__dirname, './build/public'),
@@ -14,9 +10,9 @@ module.exports = {
 		{
 			name: 'SampleApp',
 			path: path.resolve(__dirname, './src/apps/SampleApp'),
-			// Merges with "proxy" in root of the config object
 			proxy: {
-				publicPath: '/o/liferay-7-theme/js/react-apps/',
+				publicPath: '/',
+				target: 'http://localhost:8080',
 			},
 		},
 	],
