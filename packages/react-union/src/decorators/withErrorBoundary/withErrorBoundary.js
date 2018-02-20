1cecee60fd4f3bdb8dfdbcd0dff3319d1cabcd36
Václav Jančařík <vaclav.janc@gmail.com>
2018-02-20 16:04:26 +0100

    Update withErrorBoundary to use path

--- packages/react-union/src/decorators/withErrorBoundary/withErrorBoundary.js
+++ packages/react-union/src/decorators/withErrorBoundary/withErrorBoundary.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import path from 'ramda/src/path';
 
 import { ConfigShape } from '../../shapes';
 
@@ -15,7 +16,7 @@ const withErrorBoundary = WrappedComponent => {
 		}
 
 		render() {
-			const { name } = this.props.descriptor;
+			const name = path(['props', 'descriptor', 'name'], this);
 
 			if (this.state.hasError) {
 				// TODO: perhaps show some tips for common mistakes?
