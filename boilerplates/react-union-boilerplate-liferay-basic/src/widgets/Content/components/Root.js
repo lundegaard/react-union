b19a9a8943a3ff85d8ab467adf49047376771524
Tomáš Konrády <tomaskonrady@gmail.com>
2018-05-11 17:21:30 +0200

    Passing server data to content widget

--- boilerplates/react-union-boilerplate-liferay-basic/src/widgets/Content/components/Root.js
+++ boilerplates/react-union-boilerplate-liferay-basic/src/widgets/Content/components/Root.js
@@ -1,3 +1,20 @@
-const Root = () => 'I am content';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Root = ({ data: { textation } }) => (
+	<section>
+		<h1>{textation.heading}</h1>
+		<p>{textation.content}</p>
+	</section>
+);
+
+Root.propTypes = {
+	data: PropTypes.shape({
+		textation: PropTypes.shape({
+			heading: PropTypes.node,
+			content: PropTypes.node,
+		}),
+	}),
+};
 
 export default Root;
