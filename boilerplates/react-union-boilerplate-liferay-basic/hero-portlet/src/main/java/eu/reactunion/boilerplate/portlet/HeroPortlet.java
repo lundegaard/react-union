5a439cd04453dcf0fc0742291f25c0bbb86a7b4b
roman-srom <roman.srom@lundegaard.eu>
2018-05-14 21:09:50 +0200

    theme metadata and portlet title added

--- boilerplates/react-union-boilerplate-liferay-basic/hero-portlet/src/main/java/eu/reactunion/boilerplate/portlet/HeroPortlet.java
+++ boilerplates/react-union-boilerplate-liferay-basic/hero-portlet/src/main/java/eu/reactunion/boilerplate/portlet/HeroPortlet.java
@@ -16,7 +16,6 @@ import org.osgi.service.component.annotations.Component;
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.instanceable=true",
-		"javax.portlet.display-name=hero-portlet Portlet",
 		"javax.portlet.init-param.template-path=/",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + HeroPortletKeys.HERO,
