d62b734bc61b203e1cf49642e76db6706a1c1767
aizerin <l.nuzky@gmail.com>
2018-06-19 16:28:08 +0200

    migrated to new context api

--- boilerplates/react-union-boilerplate-basic/src/widgets/Content/components/Root.js
+++ boilerplates/react-union-boilerplate-basic/src/widgets/Content/components/Root.js
@@ -1,3 +1,20 @@
-const Root = () => 'I am content.';
+import React from 'react';
+import PropTypes from 'prop-types';
+import NestedComponent from './NestedComponent';
+
+const Root = ({ namespace, data }) => (
+	<div>
+		I am widget Content. With namespace: <b>{namespace}</b> and initial data:
+		<b>{JSON.stringify(data)}</b>
+		<NestedComponent />
+	</div>
+);
+
+Root.propTypes = {
+	data: PropTypes.shape({
+		foo: PropTypes.string,
+	}),
+	namespace: PropTypes.string,
+};
 
 export default Root;
