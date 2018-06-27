031d021dba58614523a6efa62714c34a08b703aa
aizerin <l.nuzky@gmail.com>
2018-06-27 11:35:27 +0200

    tests for utils

--- packages/react-union-scripts/scripts/lib/__mocks__/fs.js
+++ packages/react-union-scripts/scripts/lib/__mocks__/fs.js
@@ -1,11 +1,32 @@
 const fs = jest.genMockFromModule('../fs');
 
 let mockDirs = null;
+let mockWsPattern = null;
+let mockWsApps = null;
+let mockAppPath = null;
 
 fs.readDirs = () => mockDirs;
 
+fs.getWorkspacesPatterns = () => mockWsPattern;
+
+fs.readAllAppsFromWorkspaces = () => mockWsApps;
+
+fs.getAppPath = () => mockAppPath;
+
 fs.__setMockDirs = newMockdirs => {
 	mockDirs = newMockdirs;
+};
+
+fs.__setWsPattern = newWsPattern => {
+	mockWsPattern = newWsPattern;
+};
+
+fs.__setWsApps = newWsApps => {
+	mockWsApps = newWsApps;
+};
+
+fs.__setAppPath = newAppPath => {
+	mockAppPath = newAppPath;
 };
 
 module.exports = fs;
