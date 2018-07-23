a4777a24f88cad5ab58428698ca902c9421aa735
Václav Jančařík <vaclav.janc@gmail.com>
2018-07-23 09:23:27 +0200

    Replace react-loadable with react-universal-component

--- boilerplates/react-union-boilerplate-monorepo/packages/union-app-sample-app/src/routes.js
+++ boilerplates/react-union-boilerplate-monorepo/packages/union-app-sample-app/src/routes.js
@@ -1,20 +1,14 @@
-/* eslint-disable babel/new-cap,import/no-unresolved,import/extensions */
+/* eslint-disable import/no-unresolved,import/extensions */
 // ESLINT IS DISABLED BECAUSE DEPS ARE NOT RESOLVED IN UNION MONO REPO
-import Loadable from 'react-loadable';
+import universal from 'react-universal-component';
 
 export default [
 	{
 		path: 'hero',
-		component: Loadable({
-			loader: () => import('union-widget-hero'),
-			loading: () => null,
-		}),
+		component: universal(() => import('union-widget-hero')),
 	},
 	{
 		path: 'content',
-		component: Loadable({
-			loader: () => import('union-widget-content'),
-			loading: () => null,
-		}),
+		component: universal(() => import('union-widget-content')),
 	},
 ];
