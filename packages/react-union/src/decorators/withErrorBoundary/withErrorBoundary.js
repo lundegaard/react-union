15a894749be114488636e7591c18cf20ddf7af78
Václav Jančařík <vaclav.janc@gmail.com>
2018-07-26 14:43:33 +0200

    Add commonData support, rename name to widget internally

--- packages/react-union/src/decorators/withErrorBoundary/withErrorBoundary.js
+++ packages/react-union/src/decorators/withErrorBoundary/withErrorBoundary.js
@@ -4,7 +4,7 @@ import path from 'ramda/src/path';
 import { ConfigShape } from '../../shapes';
 import { getDisplayName } from '../../utils';
 
-const getName = path(['props', 'descriptor', 'name']);
+const getWidgetName = path(['props', 'descriptor', 'widget']);
 
 const withErrorBoundary = NextComponent => {
 	class WithErrorBoundary extends Component {
@@ -21,11 +21,11 @@ const withErrorBoundary = NextComponent => {
 		}
 
 		render() {
-			const name = getName(this);
+			const widgetName = getWidgetName(this);
 
 			if (this.state.hasError) {
 				// TODO: perhaps show some tips for common mistakes?
-				return `An error has occurred in widget "${name}". See the console output for more details.`;
+				return `An error has occurred in widget "${widgetName}". See the console output for more details.`;
 			}
 
 			return <NextComponent {...this.props} />;
