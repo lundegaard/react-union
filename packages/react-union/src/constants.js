a9a2719bc49f9de614d0a1c09f0cd7c88fb6c3e8
Václav Jančařík <vaclav.janc@gmail.com>
2018-09-26 20:11:37 +0200

    Use typeof document instead of process.env.BROWSER

--- packages/react-union/src/constants.js
+++ packages/react-union/src/constants.js
@@ -1,2 +1,3 @@
 export const INVALID_JSON = 'INVALID_JSON';
-export const SHOULD_NOT_LEAK = process.env.NODE_ENV === 'production' && process.env.BROWSER;
+export const SHOULD_NOT_LEAK =
+	process.env.NODE_ENV === 'production' && typeof document !== 'undefined';
