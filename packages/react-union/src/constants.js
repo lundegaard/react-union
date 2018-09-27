d2ca12271569aeebf5e490eae999544242c496af
Václav Jančařík <vaclav.janc@gmail.com>
2018-09-27 12:07:47 +0200

    Fix globals usage in react-union dom file

--- packages/react-union/src/constants.js
+++ packages/react-union/src/constants.js
@@ -1,3 +1,4 @@
 export const INVALID_JSON = 'INVALID_JSON';
+
 export const SHOULD_NOT_LEAK =
 	process.env.NODE_ENV === 'production' && typeof document !== 'undefined';
--- packages/react-union/src/dom.js
+++ packages/react-union/src/dom.js
@@ -6,13 +6,15 @@ const DEFAULT_UNION_ROOT_ID = 'union';
 export function justRender(reactElement, rootId = DEFAULT_UNION_ROOT_ID, resolve = noop) {
 	const domElement = createElementWithId(rootId, document.body);
 
-	if (window.__SCAN_RESULT__) {
+	if (window.__HYDRATE__) {
 		// TODO: Use ReactDOM.hydrate once React supports hydration of portals.
 		// https://github.com/facebook/react/issues/13097
 		ReactDOM.render(reactElement, domElement, (...args) => {
-			// NOTE: We delete window.__SCAN_RESULT__ so it is only used once. If the <Union /> element
+			// NOTE: We the globals so they're only used once. If the <Union /> element
 			// remounts for any reason, we want to call Union.scan() and not reuse SSR scan result.
+			delete window.__HYDRATE__;
 			delete window.__SCAN_RESULT__;
+			delete window.__INITIAL_PROPS__;
 			resolve(...args);
 		});
 	}
