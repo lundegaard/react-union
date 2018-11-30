12b8337aed41d0e84602c9e7cbf5ed107f41b1e2
Václav Jančařík <vaclav.janc@gmail.com>
2018-11-30 14:27:35 +0100

    Update liferay boilerplate to use react-union correctly

--- boilerplates/react-union-boilerplate-liferay-basic/packages/app-demo/src/index.js
+++ boilerplates/react-union-boilerplate-liferay-basic/packages/app-demo/src/index.js
@@ -6,27 +6,13 @@ import ready from 'document-ready';
 
 import Root from './components/Root';
 
-const render = Component =>
-	justRender(
-		<AppContainer errorReporter={__DEV__ ? require('redbox-react').default : null}>
-			<Component />
-		</AppContainer>
-	);
-
-const rerenderContainer = () => {
-	const NextRoot = require('./components/Root').default;
-	render(NextRoot);
-};
+const render = () => justRender(<Root />);
 
 ready(() => {
-	render(Root);
+	render();
 
 	if (window.Liferay) {
 		window.Liferay.on('startNavigate', justUnmountComponentAtNode);
-		window.Liferay.on('endNavigate', rerenderContainer);
+		window.Liferay.on('endNavigate', render);
 	}
 });
-
-if (module.hot) {
-	module.hot.accept(['./components/Root'], rerenderContainer);
-}
