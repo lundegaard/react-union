c267a25ed1a0e43ce24327504c9cc0d5916c9e34
Václav Jančařík <vaclav.janc@gmail.com>
2018-12-03 09:22:37 +0100

    Simplify IS_SERVER check

--- packages/react-union/src/constants.js
+++ packages/react-union/src/constants.js
@@ -1,7 +1,5 @@
 export const INVALID_JSON = 'INVALID_JSON';
 
-export const IS_SERVER = Boolean(
-	typeof process !== 'undefined' && process.versions && process.versions.node
-);
+export const IS_SERVER = Boolean(process && process.versions && process.versions.node);
 
 export const SHOULD_NOT_LEAK = process.env.NODE_ENV === 'production' && !IS_SERVER;
