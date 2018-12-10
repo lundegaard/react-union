94ab2af44bd5309cbd19d153197e24f68a17f2c6
Václav Jančařík <vaclav.janc@gmail.com>
2018-12-10 17:13:58 +0100

    Fix babel.config in SSR boilerplate

--- boilerplates/react-union-boilerplate-ssr-basic/babel.config.js
+++ boilerplates/react-union-boilerplate-ssr-basic/babel.config.js
@@ -1,5 +1,12 @@
-{
-  "presets": [
-    "babel-preset-react-union"
-  ]
-}
+module.exports = api => {
+	api.cache.using(() => process.env.NODE_ENV);
+
+	return api.env('test')
+		? {
+				presets: ['babel-preset-react-union'],
+				plugins: ['babel-plugin-dynamic-import-node'],
+		  }
+		: {
+				presets: ['babel-preset-react-union'],
+		  };
+};
