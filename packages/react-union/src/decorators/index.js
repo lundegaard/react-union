48f12eb72bbf6656585d8a829dc664b64b0c71df
Václav Jančařík <vaclav.janc@gmail.com>
2018-12-28 14:05:19 +0100

    Simplify PrescanContext in react-union

--- packages/react-union/src/decorators/index.js
+++ packages/react-union/src/decorators/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 
 import { getDisplayName } from '../utils';
-import { PrescanContext, WidgetContext } from '../contexts';
+import { WidgetContext } from '../contexts';
 
 export const makeContextDecorator = (Context, displayName) => NextComponent => {
 	const Decorator = props => (
@@ -15,6 +15,5 @@ export const makeContextDecorator = (Context, displayName) => NextComponent => {
 	return Decorator;
 };
 
-export const withPrescanContext = makeContextDecorator(PrescanContext, 'PrescanContext');
 export const withWidgetContext = makeContextDecorator(WidgetContext, 'WidgetContext');
 export { default as withErrorBoundary } from './withErrorBoundary';
