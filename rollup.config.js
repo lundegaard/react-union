691b24bf6839ca95840fef442073a29cd2fe1e32
Václav Jančařík <vaclav.janc@gmail.com>
2018-12-31 12:55:48 +0100

    Utilize autoExternal plugin properly in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
-import replace from 'rollup-plugin-replace';
+import replacePlugin from 'rollup-plugin-replace';
+import autoExternalPlugin from 'rollup-plugin-auto-external';
 import cjsPlugin from 'rollup-plugin-commonjs';
 import { terser as terserPlugin } from 'rollup-plugin-terser';
-import nodeResolvePlugin from 'rollup-plugin-node-resolve';
 import babelPlugin from 'rollup-plugin-babel';
 
 import path from 'path';
 import { toPascalCase, toKebabCase } from 'ramda-extension';
-import { keys } from 'ramda';
 
 const { LERNA_PACKAGE_NAME, LERNA_ROOT_PATH } = process.env;
 
 const plugins = {
+	autoExternal: autoExternalPlugin(),
 	cjs: cjsPlugin({
 		include: /node_modules/,
 	}),
@@ -22,7 +22,6 @@ const plugins = {
 			warnings: false,
 		},
 	}),
-	nodeResolve: nodeResolvePlugin(),
 	babel: babelPlugin({
 		cwd: LERNA_ROOT_PATH,
 		exclude: 'node_modules/**',
@@ -44,9 +43,6 @@ const globals = {
 	'react-union': 'ReactUnion',
 };
 
-// NOTE: We are not using `rollup-plugin-auto-external` because it includes `@babel/runtime`
-// source code in the library (instead of including an import to process when building an app).
-const external = id => keys(globals).includes(id) || id.includes('@babel/runtime');
 const globalName = toPascalCase(LERNA_PACKAGE_NAME);
 const fileName = toKebabCase(LERNA_PACKAGE_NAME);
 
@@ -54,31 +50,28 @@ export default [
 	// CJS
 	{
 		input: INPUT_FILE,
-		external,
 		output: {
 			file: path.join(PACKAGE_ROOT_PATH, 'lib', `${fileName}.js`),
 			format: 'cjs',
 			indent: false,
 		},
-		plugins: [plugins.nodeResolve, plugins.babel, plugins.cjs],
+		plugins: [plugins.babel, plugins.cjs, plugins.autoExternal],
 	},
 
 	// ES
 	{
 		input: INPUT_FILE,
-		external,
 		output: {
 			file: path.join(PACKAGE_ROOT_PATH, 'es', `${fileName}.js`),
 			format: 'es',
 			indent: false,
 		},
-		plugins: [plugins.nodeResolve, plugins.babel, plugins.cjs],
+		plugins: [plugins.babel, plugins.cjs, plugins.autoExternal],
 	},
 
 	// UMD Development
 	{
 		input: INPUT_FILE,
-		external,
 		output: {
 			file: path.join(PACKAGE_ROOT_PATH, 'dist', `${fileName}.js`),
 			format: 'umd',
@@ -87,17 +80,16 @@ export default [
 			globals,
 		},
 		plugins: [
-			replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
-			plugins.nodeResolve,
+			replacePlugin({ 'process.env.NODE_ENV': JSON.stringify('development') }),
 			plugins.babel,
 			plugins.cjs,
+			plugins.autoExternal,
 		],
 	},
 
 	// UMD Production
 	{
 		input: INPUT_FILE,
-		external,
 		output: {
 			file: path.join(PACKAGE_ROOT_PATH, 'dist', `${fileName}.min.js`),
 			format: 'umd',
@@ -106,10 +98,10 @@ export default [
 			globals,
 		},
 		plugins: [
-			replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-			plugins.nodeResolve,
+			replacePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
 			plugins.babel,
 			plugins.cjs,
+			plugins.autoExternal,
 			plugins.terser,
 		],
 	},
