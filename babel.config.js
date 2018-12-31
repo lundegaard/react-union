970ad5073ab33f6643ab59bfa87cad07562918fc
Václav Jančařík <vaclav.janc@gmail.com>
2018-12-31 10:00:07 +0100

    Refactor babel and rollup config to fix broken react-union build

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,10 @@
-const { NODE_ENV, BABEL_ENV } = process.env;
-const cjs = NODE_ENV === 'test' || BABEL_ENV === 'commonjs';
-const loose = true;
-
 module.exports = {
 	presets: [
 		[
-			'@babel/preset-env',
+			'babel-preset-react-union',
 			{
-				targets: {
-					ie: 9,
-				},
-				loose,
-				useBuiltIns: false,
-				modules: false,
+				library: true,
 			},
 		],
 	],
-	plugins: [
-		'@babel/plugin-syntax-dynamic-import',
-		['@babel/plugin-proposal-class-properties', { loose }],
-		['@babel/plugin-proposal-object-rest-spread', { loose }],
-		'@babel/plugin-transform-react-jsx',
-		cjs && ['@babel/plugin-transform-modules-commonjs', { loose }],
-	].filter(Boolean),
 };
