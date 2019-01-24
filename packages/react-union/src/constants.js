b40a0fd1225d10dc058d5c3f3ea911a94feafde4
Václav Jančařík <vaclav.janc@gmail.com>
2019-01-24 16:33:23 +0100

    Add rescan feature

--- packages/react-union/src/constants.js
+++ packages/react-union/src/constants.js
@@ -3,3 +3,5 @@ export const INVALID_JSON = 'INVALID_JSON';
 export const IS_SERVER = Boolean(process && process.versions && process.versions.node);
 
 export const SHOULD_NOT_LEAK = process.env.NODE_ENV === 'production' && !IS_SERVER;
+
+export const RESCAN = 'react-union/rescan';
