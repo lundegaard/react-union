356966ac64c9b7f52ea3b57ea18494dff220a4ca
Tomáš Konrády <tomaskonrady@gmail.com>
2019-09-27 15:34:22 +0200

    #417 liferay-ssr boilerplate for Lfr 7.2

--- boilerplates/react-union-boilerplate-liferay-ssr/tools/amdLoaderScripts.js
+++ boilerplates/react-union-boilerplate-liferay-ssr/tools/amdLoaderScripts.js
@@ -1,10 +1,7 @@
-const fs = require('fs-extra');
-const mkdirp = require('mkdirp');
 const path = require('path');
-const rimraf = require('rimraf');
+const fs = require('fs-extra');
 const glob = require('glob');
 const R = require('ramda');
-const R_ = require('ramda-extension');
 
 const appDirectory = fs.realpathSync(process.cwd());
 const srcPackagesDirectory = path.join(appDirectory, 'packages');
@@ -14,40 +11,12 @@ const getAvailableApps = R.compose(
 	R.curryN(2, glob.sync)
 );
 
-const DEBUG = !process.argv.includes('--release');
-
 const vendorFileName = 'vendor.js';
 const runtimeFileName = 'runtime.js';
 const mainFileName = 'main.js';
 const manifestFileName = 'assetManifest.json';
 
 const buildDirectory = path.join(appDirectory, 'build');
-const targetDirectory = path.join(
-	appDirectory,
-	'liferay/amd-loader/src/main/resources/META-INF/resources'
-);
-const configFilePath = path.join(buildDirectory, 'config.js');
-
-const joinNonEmpty = xs => xs.filter(Boolean).join('.');
-
-const getLiferayFilePath = (appName, hash) =>
-	path.join(buildDirectory, `${joinNonEmpty([appName, hash])}.js`);
-
-const getLiferayContextPath = (appName, hash) =>
-	`/o/liferay-amd-loader/${joinNonEmpty([appName, hash])}.js`;
-
-const createLiferayConfigSource = ({
-																		 name,
-																		 exportsIdentifier,
-																		 path = null,
-																		 dependencies = [],
-																	 }) => `Liferay.Loader.addModule({
-	dependencies: ${JSON.stringify(dependencies)},
-	name: ${JSON.stringify(name)},
-	exports: ${JSON.stringify(exportsIdentifier)},
-	path: ${JSON.stringify(path || name)},
-	type: 'js'
-});\n`;
 
 const getEntryBundlesFromManifest = manifest =>
 	R.filter(Boolean, {
@@ -56,19 +25,10 @@ const getEntryBundlesFromManifest = manifest =>
 		app: manifest[mainFileName],
 	});
 
-const getHashPart = x => (x ? x.split('.')[1] : '');
-
-const getHash = (appName, manifest) => {
-	if (DEBUG) {
-		return '';
-	}
-
-	const paths = getEntryBundlesFromManifest(manifest);
-
-	return `${getHashPart(paths.app)}${getHashPart(paths.vendor)}${getHashPart(paths.runtime)}`;
-};
-
-const getLoadScriptsSrc = R.o(R.values, R.map(x => `Liferay.Loader._loadScript({ url: '${x}' });`));
+const getLoadScriptsSrc = R.o(
+	R.values,
+	R.map(x => `Liferay.Loader._scriptLoader._loadScript({ url: '${x}' });`)
+);
 
 const joinByNewline = R.join('\n');
 
@@ -76,7 +36,7 @@ const getLoaderSource = (appName, manifest) => {
 	const paths = getEntryBundlesFromManifest(manifest);
 	const loadScripts = getLoadScriptsSrc(paths);
 
-	return `${joinByNewline(loadScripts)}\nwindow.${R_.toCamelCase(appName)} = {};\n`;
+	return `${joinByNewline(loadScripts)}\n`;
 };
 
 const createLiferayConfig = () => {
@@ -88,17 +48,11 @@ const createLiferayConfig = () => {
 	R.forEach(appName => {
 		const manifestFilePath = path.join(buildDirectory, appName, manifestFileName);
 		const manifest = fs.readJsonSync(manifestFilePath, 'utf8');
-		const hash = getHash(appName, manifest);
+		// const hash = getHash(appName, manifest);
 
-		fs.writeFileSync(getLiferayFilePath(appName, hash), getLoaderSource(appName, manifest), 'utf8');
-
-		fs.appendFileSync(
-			configFilePath,
-			createLiferayConfigSource({
-				name: appName,
-				exportsIdentifier: R_.toCamelCase(appName),
-				path: getLiferayContextPath(appName, hash),
-			}),
+		fs.writeFileSync(
+			path.join(buildDirectory, `${appName}.js`),
+			getLoaderSource(appName, manifest),
 			'utf8'
 		);
 	})(appsAvailable);
