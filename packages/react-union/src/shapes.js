3e754faa82a03f317d97f5c5c95a026bca4edde9
Tomáš Konrády <tomaskonrady@gmail.com>
2020-02-25 17:04:21 +0100

    fix propType for routeShape

--- packages/react-union/src/shapes.js
+++ packages/react-union/src/shapes.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 export const RouteShape = {
-	component: PropTypes.func.isRequired,
+	component: PropTypes.elementType.isRequired,
 	path: PropTypes.string.isRequired,
 };
 
@@ -16,7 +16,7 @@ export const WidgetConfigShape = {
 	// NOTE: The reason for this shallow structure is that we merge `commonData` into `data`.
 	// If we were changing `descriptor.data` under the hood, it would be misleading.
 	...WidgetDescriptorShape,
-	component: PropTypes.func.isRequired,
+	component: PropTypes.elementType.isRequired,
 	// NOTE: We added `isRequired` to avoid using `namespace || container` everywhere.
 	namespace: PropTypes.string.isRequired,
 };
