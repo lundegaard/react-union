2bc30127d3999e1a947bfb0322a6704b82dfb240
Tomáš Konrády <tomaskonrady@gmail.com>
2020-03-26 10:43:34 +0100

    fix: removing check for unknown route

--- packages/react-union/src/routing.js
+++ packages/react-union/src/routing.js
@@ -15,16 +15,22 @@ const findRouteByDescriptor = (routes, descriptor) =>
 
 const findComponentByDescriptor = (routes, descriptor) => {
 	const route = findRouteByDescriptor(routes, descriptor);
-	invariant(route, `Missing route for widget name ${descriptor.widget}.`);
-	return route.component;
+
+	return route ? route.component : null;
 };
 
-const makeWidgetConfig = curry((routes, commonData, descriptor) => ({
-	...descriptor,
-	component: findComponentByDescriptor(routes, descriptor),
-	data: mergeData([commonData, descriptor.data]),
-	namespace: descriptor.namespace || descriptor.container,
-}));
+const makeWidgetConfig = curry((routes, commonData, descriptor) => {
+	const component = findComponentByDescriptor(routes, descriptor);
+
+	return (
+		component && {
+			...descriptor,
+			component,
+			data: mergeData([commonData, descriptor.data]),
+			namespace: descriptor.namespace || descriptor.container,
+		}
+	);
+});
 
 const getCommonData = o(mergeData, map(prop('data')));
 
@@ -38,7 +44,9 @@ const route = (routes, scanResult) => {
 			'This is often due to a trailing comma or missing quotation marks.'
 	);
 
-	const widgetConfigs = map(makeWidgetConfig(routes, commonData), widgetDescriptors);
+	const widgetConfigs = map(makeWidgetConfig(routes, commonData), widgetDescriptors).filter(
+		Boolean
+	);
 
 	return { commonData, widgetConfigs, scanResult, routes };
 };
